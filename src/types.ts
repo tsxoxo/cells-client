3fecfc588f2280d55bea9735a5c7864a4ce2bc01
Tom Szwaja <59713582+tsxoxo@users.noreply.github.com>
2025-01-27 16:15:07 +0000

    tidy up from template

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,5 @@
-export interface Circle {
-    coordinates: { x: number, y: number },
-    radius: number,
-    id: number;
+export interface Cell {
+    content: number,
+    id: number,
+    cellsThatDependOnMe: number[]
 }
-
-export type State = Circle[]
